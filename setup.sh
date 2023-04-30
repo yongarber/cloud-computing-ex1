5fc71234504fc4799094506cccf86e881a669a3b
Yon Garber <43474593+yongarber@users.noreply.github.com>
2023-04-30 16:18:22 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# setup AWS CLI for mac- this can be installed from https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target 
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg ./AWSCLIV2.pkg -target /
+
+# Configure AWS setup (keys, region, etc)
+aws configure
+
+# install boto3 and paramiko
+pip install boto3
+pip install paramiko
