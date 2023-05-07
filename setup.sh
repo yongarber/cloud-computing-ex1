5782e06195709d3b9e32e5528ad6e58247472839
Yon Garber <43474593+yongarber@users.noreply.github.com>
2023-05-07 17:46:12 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+## We assume that you have AWS ready but if not run thw below. You have to install boto3 and paramiko in case you don't have thoose libraries.
+
 # setup AWS CLI for mac- this can be installed from https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target 
